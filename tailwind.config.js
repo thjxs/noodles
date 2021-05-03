37c28852e9b8d26d2f3ebc3c0af0e3fc92b44a22
thjxs <tanhongjian@outlook.com>
2021-05-04 01:23:32 +0800

    Fix browser issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.njk'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: colors
   },
   variants: {
     extend: {},
